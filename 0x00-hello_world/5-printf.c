fa9942f6c99b55d2fd9f8ee770499dc74e73644d
AcxelHernandez <4263@holbertonschool.com>
2022-02-03 18:56:05 +0000

    Upload new file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase,
+ * Description: task Hello, printf
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
