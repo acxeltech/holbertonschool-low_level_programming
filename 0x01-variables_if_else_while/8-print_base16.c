ef27820c7e24775cedc8ac5a8e68d2df4d329ae2
AcxelHernandez <4263@holbertonschool.com>
2022-02-09 17:42:38 +0000

    Upload new file

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all the numbers of base 16 in lowercase
+ * followed by a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+	char chr;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+	}
+	for (chr = 'a'; chr <= 'f'; chr++)
+	{
+		putchar(chr);
+	}
+	putchar('\n');
+	return (0);
+}	
