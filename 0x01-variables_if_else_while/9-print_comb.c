6272bf1b6d35d96faa3fd6c1f2729c1a5c51c103
AcxelHernandez <4263@holbertonschool.com>
2022-02-09 17:53:46 +0000

    new upload file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Program prints all possible combinations
+ * of single-digit numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+		if (num != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
